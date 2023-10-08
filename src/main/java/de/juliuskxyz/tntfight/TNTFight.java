e14ed183919292e20dc8cb8cdc672ee349af544f
Julius <frayyze6@gmail.com>
2023-10-08 19:57:26 +0200

    Initial commit

---
+++ src/main/java/de/juliuskxyz/tntfight/TNTFight.java
@@ -0,0 +1,17 @@
+package de.juliuskxyz.tntfight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TNTFight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
