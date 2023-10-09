b2621d6925783019e89d3f105b1eb1dd1a33ddc5
Julius <frayyze6@gmail.com>
2023-10-09 20:44:51 +0200

    Added PlayerDeathListener.kt

--- src/main/java/de/juliuskxyz/tntfight/TNTFight.kt
+++ src/main/java/de/juliuskxyz/tntfight/TNTFight.kt
@@ -3,7 +3,6 @@ package de.juliuskxyz.tntfight
 import de.juliuskxyz.tntfight.listeners.PlayerBreakBlockEvent
 import de.juliuskxyz.tntfight.listeners.PlayerDeathListener
 import de.juliuskxyz.tntfight.listeners.PlayerJoinListener
-import de.juliuskxyz.tntfight.listeners.SandBotListener
 import org.bukkit.plugin.java.JavaPlugin
 
 class TNTFight : JavaPlugin() {
@@ -21,26 +20,11 @@ class TNTFight : JavaPlugin() {
 
         config.options().copyDefaults()
         saveDefaultConfig()
-
-        instance = this // here set the instance of this, so as the current object
-
-        // it will make this object available for everyone
-        // it will make this object available for everyone
-        server.pluginManager.registerEvents(SandBotListener(this), this);
-
     }
     override fun onDisable() {
         // Plugin shutdown logic
         println("------------------")
         println("TNT Fight disabled!")
         println("------------------")
-    }
-
-    private var instance // create variable that is "static".
-            : TNTFight? = null
-
-    // It means it not depend to an object to get it
-    fun getInstance(): TNTFight? { // get the object instance, so the plugin instance
-        return instance
     }
 }
---
+++ src/main/java/de/juliuskxyz/tntfight/listeners/EntityShootBowEvent.java
@@ -0,0 +1,16 @@
+package de.juliuskxyz.tntfight.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class EntityShootBowEvent implements Listener {
+
+    @EventHandler
+    public void onFireBow(org.bukkit.event.entity.EntityShootBowEvent e){
+
+        Player p  = (Player)
+
+    }
+
+}
