7cc08a8b370d1d9ff70745c07f86f11bc1779f45
Michele <michele.sauer.2607@gmail.com>
2023-10-09 18:49:55 +0200

    Added new PlayerJoinListener

--- src/main/java/de/juliuskxyz/tntfight/listeners/PlayerDeathListener.kt
+++ src/main/java/de/juliuskxyz/tntfight/listeners/PlayerDeathListener.kt
@@ -1,14 +1,19 @@
 package de.juliuskxyz.tntfight.listeners
 
-import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.GameMode
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
 
 class PlayerDeathListener : Listener {
     @EventHandler
-    fun onPlayerDeath(e: PlayerDeathEvent) {
-        val p = e.player
-        e.player.gameMode = GameMode.SPECTATOR
+    fun onPlayerDeath(event: EntityDamageEvent) {
+        if (event.entity is Player) {
+            val p = event.entity as Player
+            if (p.health <= 0) {
+                p.gameMode = GameMode.SPECTATOR
+            }
+        }
     }
 }
